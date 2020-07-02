ed2730629d3502ce01e80d5298cc4dbd720b72ae
ClearLiF <l18512863192@gmail.com>
2020-07-02 10:42:40 +0800

    test

---
+++ src/main/java/com/cuit/mapper/Temp.java
@@ -0,0 +1,22 @@
+package com.cuit.mapper;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * temp
+ * @author 
+ */
+@Data
+public class Temp implements Serializable {
+    private String id;
+
+    private String name;
+
+    private String people;
+
+    private static final long serialVersionUID = 1L;
+
+
+}
