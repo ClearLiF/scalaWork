9487f9dea7636a4754724ef77dfed88ef0fc07b4
ClearLiF <l18512863192@gmail.com>
2020-07-05 22:29:09 +0800

    Revert "分支"
    This reverts commit 178a93cf

--- src/main/java/com/cuit/App.java
+++ src/main/java/com/cuit/App.java
@@ -15,6 +15,7 @@ import org.springframework.context.annotation.ComponentScan;
 @MapperScan(basePackages = {"com.cuit", "generator"})
 public class App {
     public static void main(String[] args) {
+        System.out.println();
        // System.out.println("Hello World!");
         SpringApplication.run(App.class, args);
 
